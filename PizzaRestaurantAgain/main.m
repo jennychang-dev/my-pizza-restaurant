e9ecf1b48f0a4d0f3e574eb08b3ab02e1c911f89
Jenny Chang <chang.jc95@gmail.com>
2019-01-11 23:28:21 -0800

    add updated main.m file

--- PizzaRestaurantAgain/main.m
+++ PizzaRestaurantAgain/main.m
@@ -10,10 +10,12 @@
 #import "Pizza.h"
 #import "Kitchen.h"
 #import "Conversion.h"
+#import "Manager.h"
+#import "SecondManager.h"
 
 int main(int argc, const char * argv[])
 {
-
+    
     @autoreleasepool {
         
         while (TRUE) {
@@ -27,24 +29,40 @@ int main(int argc, const char * argv[])
             
             NSLog(@"Input was %@", inputString);
             
+            NSLog(@"Now would you like a grumpy manager or a cheerful manager?");
+            char gimme[100];
+            fgets(gimme,100,stdin);
+            
             // Take the first word of the command as the size, and the rest as the toppings
-            NSString *size = [[inputString componentsSeparatedByString:@" "] objectAtIndex:0];
+            NSString *size = [[inputString componentsSeparatedByString:@" "] objectAtIndex:0]; // this gives me "small" as a string
             
             Conversion *newConversion = [[Conversion alloc] init];
             PizzaSize sizeEnum = [newConversion convertStringToEnum:size];
-            // this will return pizza size
-
+            // this will return pizza size form
+            
             NSString *removeSize = [inputString stringByReplacingOccurrencesOfString:size withString:@""];
             NSString *requestedToppings = [removeSize stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-            NSArray *toppings = [requestedToppings componentsSeparatedByString:@" "];
+            NSArray *toppings = [requestedToppings componentsSeparatedByString:@" "]; // this gives me an array of toppigns
             
+            // below calls the kitch to make pizza
             Kitchen *restaurantKitchen = [[Kitchen alloc] init];
-            Pizza *p = [restaurantKitchen makePizzaWithSize:sizeEnum toppings:toppings];
-            if ([p size] ==  medium){
-                NSLog(@"create medium pizza");
-            }
+            [restaurantKitchen makePizzaWithSize:sizeEnum toppings:toppings];
+            
+            // come with a switch statement that takes a string and sets delegate
+            NSString *inputManagerType = [[NSString alloc] initWithUTF8String:gimme];
+            inputManagerType = [inputManagerType stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+            
+            Manager *mdel = [[Manager alloc] init];
+            SecondManager *sdel = [[SecondManager alloc] init];
+            
+            if ([inputManagerType isEqualToString:@"grumpy"]) {
+                restaurantKitchen.delegate = mdel;
+            } else if
+                ([inputManagerType isEqualToString:@"cheery"]) {
+                    restaurantKitchen.delegate = sdel;
+            } 
+        }
+        return 0;
     }
-    return 0;
-}
 }
 
